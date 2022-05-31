349bc351d70f6e30c02af4fffa7c278c130866fe
patrykmagnus <patryk.magnus@o2.pl>
2022-05-31 21:11:22 +0200

    rozbudowa aplikacji

--- app.py
+++ app.py
@@ -20,24 +20,23 @@ def gallery():
     return render_template('gallery.html')
 
 
-
-@app.route('/error_denied')
-def error_denied():
-    abort(401)
-
-@app.route('/error_internal')
-def error_internal():
-    return render_template('template.html', name='ERROR 505'), 505
-
-@app.route('/error_not_found')
-def error_not_found():
-    response = make_response(render_template('template.html', name='ERROR 404'), 404)
-    response.headers['X-Something'] = 'A value'
-    return response
-
-@app.errorhandler(404)
-def not_found_error(error):
-    return render_template('404.html'), 404
+# @app.route('/error_denied')
+# def error_denied():
+#     abort(401)
+#
+# @app.route('/error_internal')
+# def error_internal():
+#     return render_template('template.html', name='ERROR 505'), 505
+#
+# @app.route('/error_not_found')
+# def error_not_found():
+#     response = make_response(render_template('template.html', name='ERROR 404'), 404)
+#     response.headers['X-Something'] = 'A value'
+#     return response
+#
+# @app.errorhandler(404)
+# def not_found_error(error):
+#     return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
